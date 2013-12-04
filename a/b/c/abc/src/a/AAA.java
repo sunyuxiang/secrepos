41e2032ab63079ab3f4e70adc62884fc79261fbe
sunyuxiang <785807239@qq.com>
2013-12-04 10:39:01 +0800

    ooo

---
+++ a/b/c/abc/src/a/AAA.java
@@ -0,0 +1,13 @@
+package a;
+
+public class AAA {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(444);
+	}
+
+}
